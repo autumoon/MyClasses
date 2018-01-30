4f3037980983aa29d1842b5a070e7419adcaf453
秋月的私语 <autumoon@vip.qq.com>
2018-01-30 17:25:09 +0800

    add UNICODE and MultiByte support
    when i change my .sln to MultiByte, i found some mistakes so i fix it.

--- src/Defines.h
+++ src/Defines.h
@@ -7,3 +7,17 @@
 #define INI_REMPATH			_T("INI_REMPATH")
 #define INI_PATH_DIRS		_T("INI_PATH_DIRS")
 #define INI_PATH_FILES	_T("INI_PATH_FILES")
+
+#ifdef _UNICODE
+
+#ifndef _tstring
+#define _tstring std::wstring
+#endif // _tstring
+
+#else
+
+#ifndef _tstring
+#define _tstring std::string
+#endif // _tstring
+
+#endif
