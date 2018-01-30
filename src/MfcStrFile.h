a7625f8d0669e273fa1661743256782c713aaeec
秋月的私语 <autumoon@vip.qq.com>
2018-01-30 17:27:04 +0800

    add UNICODE and MultiByte support
    when i change my .sln to MultiByte, i found some mistakes so i fix it.

--- src/MfcStrFile.h
+++ src/MfcStrFile.h
@@ -11,22 +11,37 @@
 
 #include <afxdlgs.h>        //打开文件
 #include <ShlObj.h>         //浏览文件夹
-#include <string>
+#include <string>			//std::string
+#include <tchar.h>			//_T
+
+#ifdef _UNICODE
+
+#ifndef _tstring
+#define _tstring std::wstring
+#endif // _tstring
+
+#else
+
+#ifndef _tstring
+#define _tstring std::string
+#endif // _tstring
+
+#endif
 
 class CMfcStrFile
 {
 public:
-	static std::string CString2string(const CString& strCstring);												//使用这个则不需要考虑内存释放的问题
+	static _tstring CString2string(const CString& strCstring);												//使用这个则不需要考虑内存释放的问题
 
 	static CString BrowseDir(const CString& strTips = CString("请选择文件夹"));                                //浏览一个文件夹
 	static CString BrowseDirNew(const CString& strTips = CString("请选择文件夹"));                             //浏览一个文件夹，带新建按钮
 	static CString OpenFile();
-	static CString OpenSuffixFile(const std::string& strSuffix);
+	static CString OpenSuffixFile(const _tstring& strSuffix);
 	static CString OpenSuffixFile(const int& nSuffix, ...);
-	static CString SaveSuffixFile(const std::string& strSuffix, const std::string& strDefaultName = "autumoon");
-	static size_t Split(const CString& str, CStringArray& Arr, const char& ch = ',');
+	static CString SaveSuffixFile(const _tstring& strSuffix, const _tstring& strDefaultName = _T("autumoon"));
+	static size_t Split(const CString& str, CStringArray& Arr, const TCHAR& ch = ',');
 	static size_t Split(const CString& str, CStringArray& Arr, const CString& pattern = _T(","));
 
 private:
-	static inline char* CStringToChar(const CString& strCstring);														//内部申请了内存空间，注意释放
+	static inline TCHAR* CStringToChar(const CString& strCstring);														//内部申请了内存空间，注意释放
 };
